0e5b68a6e48c75cea2108a678ef978bf9124ef7c
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2022-01-28 12:40:10 -0300

    implementacao do put e delet

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,8 +19,20 @@ export class TemaService {
     return this.http.get<Tema[]>('https://approbert.herokuapp.com/temas', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://approbert.herokuapp.com/temas/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://approbert.herokuapp.com/temas', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://approbert.herokuapp.com/temas', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://approbert.herokuapp.com/temas/${id}`, this.token)
   }
 
 }
